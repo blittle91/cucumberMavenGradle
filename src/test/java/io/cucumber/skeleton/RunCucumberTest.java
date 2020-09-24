5965978f4f7c2211eada39ad6ca59086edcae070
Ben Little <ben.little@skybettingandgaming.com>
2020-09-24 13:16:16 +0100

    Point cucumber report to a JSON formatted file

--- src/test/java/io/cucumber/skeleton/RunCucumberTest.java
+++ src/test/java/io/cucumber/skeleton/RunCucumberTest.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "json:target/cucumber-reports"})
+@CucumberOptions(plugin = {"pretty", "json:target/cucumber-reports/report.json"})
 public class RunCucumberTest {
 }
